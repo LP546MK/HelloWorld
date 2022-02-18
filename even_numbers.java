0883ce04366433b1350f5679335f4aad6b5e07f7
LP546MK <98611516+LP546MK@users.noreply.github.com>
2022-02-18 15:02:37 +0100

    Add files via upload

---
+++ even_numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class even_numbers {
+
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+        //number
+		
+		int number;
+	        Scanner	num = new Scanner(System.in);
+		    System.out.println("TYPE A NUMBER");
+		    number = num.nextInt();
+		    //print all even numbers <=n
+		    int even=2;
+		    while (even <= number) {
+		    System.out.println(even+" ");
+		    even += 2;
+		 }  
+	}
+ } 
+
