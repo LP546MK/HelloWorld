e0f9399a1893d6e12878cfd2ff36bb7fd78baf55
LP546MK <98611516+LP546MK@users.noreply.github.com>
2022-02-22 17:57:05 +0100

    Add files via upload

---
+++ even_numbers.java
@@ -0,0 +1,17 @@
+
+
+public class even_numbers {
+
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+        //number
+//		
+		 for (int num = 0; num < 100; num++) {
+			 if (num % 2 == 0) {
+				 System.out.println(num);
+			 }
+		 }
+	}
+ } 
+
